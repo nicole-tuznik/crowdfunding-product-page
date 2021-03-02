d10e2eb428971c737fe169247859f675ca6c35ed
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-03-02 15:59:59 -0500

    Main styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
 
 // compile scss into css
 function style() {
   // 1. Where is my scss file
-  return gulp.src('./scss/**/*.scss')
-  // 2. Pass that file through sass compiler
-  .pipe(sass().on('error', sass.logError))
-  // 3. Where do I save the compiled CSS
-  .pipe(gulp.dest('./css'))
-  // 4. Stream changes to all browsers
-  .pipe(browserSync.stream())
+  return (
+    gulp
+      .src("./scss/**/*.scss")
+      // 2. Pass that file through sass compiler
+      .pipe(sass().on("error", sass.logError))
+      // 3. Where do I save the compiled CSS
+      .pipe(gulp.dest("./css"))
+      // 4. Stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: './'
-    }
+      baseDir: "./",
+    },
   });
-  gulp.watch('./scss/**/*.scss', style);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
