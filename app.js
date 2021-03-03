c424b32dc99a49306d57c12273fadce4cfd180aa
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-03-03 15:32:33 -0500

    Added pledge modal

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Show nav in mobile
+
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav__links");
 
@@ -9,4 +11,20 @@ hamburger.addEventListener("click", function () {
   } else {
     hamburger.src = "images/icon-hamburger.svg";
   }
+});
+
+// Modals - pledge modal
+
+let rewardBtns = document.querySelectorAll(".reward__btn");
+const closeBtn = document.querySelector(".selection-modal__close");
+const selectionModal = document.querySelector(".selection-modal");
+
+rewardBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    selectionModal.classList.toggle("closed");
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  selectionModal.classList.add("closed");
 });
